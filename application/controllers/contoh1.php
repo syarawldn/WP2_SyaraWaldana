e49e04913ba83db0e0582bd640ff64bff8d122a5
syarawldn <waldanasyara@gmail.com>
2021-09-29 11:05:28 +0700

    pertemuan 1 dan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Syara Waldana
+            Saya tingga di daerah Jakarta
+            olah raga yang saya sukai adalah
+            Voli";
+    }
+}
