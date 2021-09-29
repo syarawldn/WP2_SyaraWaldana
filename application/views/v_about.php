5fcc53f41968a8f0829fb67511a030be5a4f134a
syarawldn <waldanasyara@gmail.com>
2021-09-29 11:52:39 +0700

    pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,30 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Syara </li>
+        <li>Nama Belakang : Waldana</li>
+    </ul>
+    
+    <h4>Nim</h4>
+    <ul type="disc">
+        <li>19200866</li>
+    </ul>
+    
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Utan Kayu Utara, Matraman, jakarta timur, Dki Jakarta</li>
+    </ul>
+
+    <h4>Tempat, Tanggal Lahir</h4>
+    <ul type="none">
+        <li>Gunung Kidul, 14 Januari 2001</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Voli</li>
+    </ul>
+</section>
